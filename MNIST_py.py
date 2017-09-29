a6cfed2b67c2b8269125a57647f187eebdac9a09
PedroHOMota <phomota@hotmail.com>
2017-09-29 16:35:35 +0100

    Added method to donwload and uncompress the training sets

--- MNIST_py.py
+++ MNIST_py.py
@@ -1,38 +1,65 @@
 import struct
 from PIL import Image
+import PIL as pil
+import gzip
+import urllib.request as rs
+import os
+
 
 #ToDo
 #Implement download the data and extracting it
+def DonwloadFile():
+    if not os.path.exists("Downloads"):
+        os.makedirs("Downloads")
+    if not os.path.isfile("Downloads//trainIMG.gz"):
+        imgD=rs.urlretrieve("http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz", "Downloads//trainIMG.gz")
 
-def extractIMG(numberOfImages): #variable will be used to dictate the number of imgs to be extracted
+    if not os.path.isfile("Downloads//trainLBL.gz"):
+        lblsD=rs.urlretrieve("http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz", "Downloads//trainLBL.gz")
+
+    with open("Downloads//trainIMG.gz","rb") as f:
+        img=f.read()
+        img=gzip.decompress(img)
+        f.close()
+
+    with open("Downloads//train-image","wb") as output:
+        output.write(img)
+        output.close()
+
+    with open("Downloads//trainLBL.gz","rb") as f:
+        img=f.read()
+        img=gzip.decompress(img)
+        f.close()
+
+    with open("Downloads//train-label","wb") as output:
+        output.write(img)
+        output.close()
+   
+
+
+def extractIMG(): #variable will be used to dictate the number of imgs to be extracted
     with open("trainIMG.idx3-ubyte","rb") as f:
         byte=f.read(16) #Reading the magical number, number of images, number of rows and number of columns at once
         mn,ni,nr,nc=struct.unpack(">IIII",byte) #Converting to big endian; Each I represents 4 big endian bytes
         img=[]
-        
-            
-        for n in range(0,numberOfImages):
-            pics=[]
-            for i in range(0,28):
-                s=[]
-                for j in range(0,28):
-                    byte=f.read(1)
-                    s.insert(j,int.from_bytes(byte,byteorder='big')) #Convert bytes to int using the Big Endian
-                pics.insert(i,s)   
-            img.insert(n,pics)
-
-    f.closed()
+        print("Magic Number: "+str(mn))
+        print("Number of Imgs: "+str(ni))
+        print("Number of rows: "+str(nr))
+        print("Number of cols: "+str(nc))
+         
+        img=[[[int.from_bytes(f.read(1),byteorder='big') for j in range (28)] for i in range(28)] for n in range(129)]
+   
+    #f.closed()
     return img
 
-def extractLBL(numberOfLabes):
+def extractLBL():
     with open("train-labels.idx1-ubyte","rb") as f:
         byte=f.read(8)
         mn,ni=struct.unpack(">II",byte)
         print(mn,ni)
-        labels=[]
-        for n in range(0,ni):
-            labels.__add__(int.from_bytes(f.read(1),byteorder='big')) #Convert byte to big endian int
-    f.closed()
+        labels=[int.from_bytes(f.read(1),byteorder='big') for n in range(ni)]
+        
+    #f.closed()
     return labels
 
 def printIMG(imgArray,imgN): #Print the image to  the console
@@ -45,23 +72,27 @@ def printIMG(imgArray,imgN): #Print the image to  the console
                 print(".", end='')
         print('\n')
 
-def SaveIMG(imgName,imgArray,imgNumber): #Convert the array and save it as a png image
+def SaveIMG(imgName,imgArray): #Convert the array and save it as a png image
     arr=[]
     a=0
     for i in range(0,28): #Transform the 2d into a 1d
             for j in range(0,28):
-                arr.insert(a,imgArray[imgNumber][i][j])
+                arr.insert(a,imgArray[i][j])
                 a=a+1
 
     im=Image.new("L",(28,28)) #B&W
-    im.putdata(arr)
-    im.save(imgName+".png","PNG")
-
+    #im.putdata(arr)
+    img=pil.fromarray(imgArray)
+    im.save("test-"+imgName+".png","PNG")
+    #"train-%05i-%i.png" % (i,50)
  
-
-imgs=extractIMG(3)
-printIMG(imgs,1)
-SaveIMG("atest2",imgs,1)
+DonwloadFile()
+imgs=extractIMG()
+lbl=extractLBL()
+printIMG(imgs,128)
+print("Started saving")
+for n in range(len(imgs)):
+    SaveIMG(n+"-"+lbl[n],imgs[n])
 
 #Reference
 #https://stackoverflow.com/questions/39969045/parsing-yann-lecuns-mnist-idx-file-formatv
